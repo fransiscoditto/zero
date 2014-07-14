2a96af221a0b2c47c7a734657bf5829cd65b342a
Fransisco Assisi Adiananta <fransiscoditto@rocketmail.com>
2014-07-14 11:41:13 +0700

    Home Page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,8 @@
 class PagesController < ApplicationController
   
   def home
-  
+  @blogs = Blog.order("created_at desc").limit(5)
+  @photos = Photo.order("created_at desc").limit(6)
   end
   
   def mv
