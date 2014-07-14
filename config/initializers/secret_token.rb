94f01e78b6f39baea6cfe4ef4ad0da4924963399
Fransisco Assisi Adiananta <fransiscoditto@rocketmail.com>
2014-07-14 07:44:00 +0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zero::Application.config.secret_token = 'b1489227fd9fc604bea209775b93ac9388bab10f2ae1309d38eba66f6276da3cf9573512bc9a59da434f3452a34dc6c799d7b7340c4ce27d2b2909eda39dfd76'
